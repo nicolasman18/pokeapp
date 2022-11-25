12fa44bf8c3ec14c94b11e79a10ed459bf1b99f8
nicolas mansilla <nicomansilla055@gmail.com>
2022-11-24 21:41:46 -0300

    cambio

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ import { NextUIProvider } from '@nextui-org/react';
 function MyApp({ Component, pageProps }) {
   return (
     // 2. Use at the root of your app
-    <NextUIProvider theme={darkTheme}>
+    <NextUIProvider>
 
       <Component {...pageProps} />
     </NextUIProvider>
